c6f03259b2da3a220c51199daf43b067f4572f6f
Jorge Betancur <jabetancur12@gmail.com>
2026-02-08 22:26:17 -0500

    fix(api): include production paths for entities in mikro-orm config

--- apps/api/src/mikro-orm.config.ts
+++ apps/api/src/mikro-orm.config.ts
@@ -5,7 +5,7 @@ import 'dotenv/config';
 const config: Options = {
     driver: PostgreSqlDriver,
     clientUrl: process.env.DATABASE_URL || 'postgresql://postgres:postgres@localhost:5433/scaffold_db',
-    entities: ['dist/**/*.entity.js'],
+    entities: ['dist/**/*.entity.js', 'apps/api/dist/**/*.entity.js'],
     entitiesTs: ['src/**/*.entity.ts'],
     metadataProvider: TsMorphMetadataProvider,
     debug: process.env.NODE_ENV === 'development',
