1f3eda3257b071169cb0c3e447f21d921e33ccb1
Jorge Betancur <jabetancur12@gmail.com>
2026-02-08 22:51:41 -0500

    fix(api): use REDIS_URL env var if available

--- apps/api/src/config/redis.ts
+++ apps/api/src/config/redis.ts
@@ -2,7 +2,7 @@ import { createClient } from 'redis';
 import { winstonLogger } from './logger';
 
 const redisClient = createClient({
-    url: `redis://${process.env.REDIS_HOST || 'localhost'}:${process.env.REDIS_PORT || 6379}`
+    url: process.env.REDIS_URL || `redis://${process.env.REDIS_HOST || 'localhost'}:${process.env.REDIS_PORT || 6379}`
 });
 
 redisClient.on('error', (err) => winstonLogger.error('Redis Client Error', err));
